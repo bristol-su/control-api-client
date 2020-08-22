5398a18b99972ee7c2f8ba112a7034a0f7c93ce3
Toby Twigger <tobytwigger1@gmail.com>
2020-08-22 21:23:58 +0100

    Updates

--- src/Client.php
+++ src/Client.php
@@ -1,6 +1,21 @@
 <?php
 
-class Client implements \BristolSU\ApiToolkit\Contracts\ClientResourceGroup
+namespace BristolSU\ApiClientImplementation\Control;
+
+use BristolSU\ApiClientImplementation\Control\Clients\GroupClient;
+
+class Client extends \BristolSU\ApiToolkit\Contracts\ClientResourceGroup
 {
 
+    public static function getMethodName(): string
+    {
+        return 'control';
+    }
+
+    public static function getResources(): array
+    {
+        return [
+          'group' => GroupClient::class
+        ];
+    }
 }
---
+++ src/Clients/GroupClient.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BristolSU\ApiClientImplementation\Control\Clients;
+
+class GroupClient extends \BristolSU\ApiToolkit\Contracts\ClientResource
+{
+
+    public function __construct()
+    {
+
+    }
+
+    public function getAll()
+    {
+
+    }
+
+}
