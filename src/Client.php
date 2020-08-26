463d1f6699fb544d502657430d5cc8e04071de2e
Toby Twigger <tobytwigger1@gmail.com>
2020-08-26 17:16:44 +0100

    Updates

--- src/Client.php
+++ src/Client.php
@@ -3,8 +3,9 @@
 namespace BristolSU\ApiClientImplementation\Control;
 
 use BristolSU\ApiClientImplementation\Control\Clients\GroupClient;
+use BristolSU\ApiToolkit\Contracts\ClientResourceGroup;
 
-class Client extends \BristolSU\ApiToolkit\Contracts\ClientResourceGroup
+class Client extends ClientResourceGroup
 {
 
     public static function getMethodName(): string
